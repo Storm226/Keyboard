cdbf61fd95e28d56584951645a96accc1052b172
DuckKee <sthing2002@gmail.com>
2025-04-10 16:17:15 -0600

    Hopefully finished fragment

--- waterRendering.fs
+++ waterRendering.fs
@@ -3,6 +3,7 @@
 out vec4 color;
 
 in vec2 texCoord;
+in vec3 fragPosWorld;
 in vec3 cameraPos;
 
 uniform vec3 lightPosWorld;
@@ -11,50 +12,45 @@ uniform vec3 objectColor;
 uniform mat3 View;
 
 uniform sampler2D normalMap;
+uniform sampler2D reflectionMap;
 
 void main() {
     vec3 normalColor = texture(normalMap, texCoord).rgb;
+    vec3 norm = normalize(normalColor);
+
     vec3 lightPos = vec3(View * lightPosWorld);
 
     // ambient
     float ambientStrength = 0.1;
     //vec3 ambient = ambientStrength * lightColor * diffuseColor;
     vec3 ambient = ambientStrength * lightColor;
-  	
+
     // diffuse 
-    vec3 norm = normalize(normalColor);
-    vec3 lightDir = normalize(lightPos - cameraPos);
+    vec3 lightDir = normalize(lightPos - fragPosWorld);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
     
-    // specular
+    // specular - blinn-phong
     float specularStrength = 0.5;
-    vec3 viewDir = normalize(0 - cameraPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 viewDir = normalize(cameraPos - fragPosWorld);
+    vec3 halfDir = normalize(lightDir + viewDir);
+    //vec3 reflectDir = reflect(-lightDir, norm);  
+    //float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    float spec = pow(max(dot(norm, halfDir), 0.0), 32.0) //shiniess is 3
     vec3 specular = specularStrength * spec * lightColor;  
         
     vec3 result = (ambient + diffuse + specular) * objectColor;
 
-    //color = vec4(result, 1.0);
-    //color = vec4(normalColor, 1.0);
+    // fresnel
+    float fresnel = pow(1.0 - clamp(dot(viewDir, normal), 0.0, 1.0), 5.0);
+
+    //disort reflection w normal
+    vec2 distortion = norm.xz * 0.02;
+    vec2 distortionUV = texCoord + distortion;
+    vec3 reflectionColor = texture(reflectionMap, distortionUV).rgb;
+
+    vec3 finalcolor = mix(litColor, reflectionColor, fresnel);
+
+    //color = vec4(finalcolor, 1.0);
     color = vec4(1, 0, 0, 1);
-}
-
-//Vertex
-#version 410 core
-
-layout(location = 0) in vec3 planePOS;
-layout(location = 1) in vec2 planeTexCoord;
-
-uniform mat4 planeMVP;
-
-out vec3 modelPos;
-out vec2 tesTextureCoordOut;
-
-void main() {
-    tesTextureCoordOut = planeTexCoord;
-    modelPos = planePOS;
-    tesTextureCoordOut.y = -tesTextureCoordOut.y;
-    gl_Position = planeMVP * vec4(planePOS, 1.0);
 }
